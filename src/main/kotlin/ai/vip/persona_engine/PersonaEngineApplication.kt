20702cb30b0f446f9b7c43c7f8608d359ec7453d
권장순 <jsoonworld@gmail.com>
2025-09-25 19:21:32 +0900

    feat: init

---
+++ src/main/kotlin/ai/vip/persona_engine/PersonaEngineApplication.kt
@@ -0,0 +1,11 @@
+package ai.vip.persona_engine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonaEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<PersonaEngineApplication>(*args)
+}
---
+++ src/test/kotlin/ai/vip/persona_engine/PersonaEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package ai.vip.persona_engine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonaEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
